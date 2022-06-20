42ff8443f1bd04732b010c5daea2a13724c361e0
Nada <67028272+nadaAlruwaythi@users.noreply.github.com>
2022-06-20 15:39:17 +0300

    Add files via upload

---
+++ alexa.js
@@ -0,0 +1,36 @@
+const startButton=document.querySelector('#start');
+const recognition=new webkitSpeechRecognition;
+recognition.continous=false;
+recognition.lang="en-US";
+recognition.interimResults=false;
+recognition.maxAlternative=1;
+const synth=window.speechSynthesis;
+
+
+startButton.addEventListener("click", () =>{
+     recognition.start();
+
+});
+
+let utter =new SpeechSynthesisUtterance("Hi, how are you?");
+utter.onend=()=>{ 
+     recognition.start();
+     };
+
+recognition.onresults=(e)=> {
+     const transcript=e.results[e.results.length-1] [0].transcript.trim(); 
+     if(transcript="hey")
+     {
+        recognition.stop(); 
+        utter.text="Hi,how are you? ";
+        synth.speak(utter);
+        console.log('hi,how are you');
+     }
+     else if(transcript==='goodbye')
+     {  recognition.stop(); 
+        utter.text="okay bye";
+        synth.speak(utter);
+        console.log('okay, see soon');
+
+     }
+    }
